78c27b831df72a94d8a7364e4cde6e51d21778fd
Francisco <fnaveira11@gmail.com>
2021-03-31 20:28:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Your code here
+function createEmployeeRecord([string, stringTwo, stringThree, num]){
+    const employeeObject = {firstName: string, familyName: stringTwo, title: stringThree, payPerHour: num, timeInEvents: [], timeOutEvents: [] }
+    return employeeObject
+}
+
+function createEmployeeRecords(arry){
+    return arry.map( x => createEmployeeRecord(x))
+}
+
+function createTimeInEvent(object, stamp) {
+    let splitStamp = stamp.split(" ")
+    let hour = splitStamp[1]
+    let date = splitStamp[0]
+    object.timeInEvents.push({
+        type:"TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return object
+}
+
+function createTimeOutEvent(object, stamp) {
+    let splitStamp = stamp.split(" ")
+    let hour = splitStamp[1]
+    let date = splitStamp[0]
+    object.timeOutEvents.push({
+        type:"TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return object
+}
+
+function hoursWorkedOnDate(object, date) {
+    const timeInArry = object.timeInEvents
+    const timeOutArry = object.timeOutEvents
+    const findDateIn = timeInArry.find(element => element.date === date)
+    const findDateOut = timeOutArry.find(element => element.date === date)
+    return (findDateOut.hour - findDateIn.hour)/100
+}
+
+function wagesEarnedOnDate(object, date ) {
+    return hoursWorkedOnDate(object, date) * object.payPerHour
+}
+
+function allWagesFor(object) {
+    const timeInArry  = object.timeInEvents
+    const wages = timeInArry.reduce((memo, element)=> {
+        return memo += wagesEarnedOnDate(object,element.date)
+        },0)
+    return wages 
+}
+
+function findEmployeeByFirstName(srcArry, firstName) {
+    const idk = srcArry[0][0]
+    console.log("------------------------------------------------------------------")
+    console.log(idk)
+    console.log(firstName)
+    console.log("------------------------------------------------------------------")
+}
+
+function calculatePayroll(empArry) {
+    const findTimeInEvent = empArry.find( element => {
+        element.timeInEvents === timeInEvents
+    })
+    console.log('--------------------------------------------------------')
+    console.log(findTimeInEvent)
+    console.log('--------------------------------------------------------')
+}
